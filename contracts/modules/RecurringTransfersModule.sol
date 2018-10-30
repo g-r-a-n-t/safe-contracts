92d4ec58d464510cf312f067bf56cab26976569c
Grant Wuerker <gwuerker@googlemail.com>
2018-10-30 18:16:43 -0500

    empty recurrint transfers contract created

---
+++ contracts/modules/RecurringTransfersModule.sol
@@ -0,0 +1,13 @@
+pragma solidity 0.4.24;
+import "../base/Module.sol";
+import "../base/ModuleManager.sol";
+import "../base/OwnerManager.sol";
+import "../common/Enum.sol";
+
+/// @title Recurring Transfer Module - Allows an owner...
+/// @author Grant Wuerker - <gwuerker@gmail.com>
+contract RecurringTransfersModule is Module {
+    string public constant NAME = "Recurring Transfers Module";
+    string public constant VERSION = "0.0.2";
+
+}
