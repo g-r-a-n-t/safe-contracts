35498eca8888c8140da0579a1a087b4c028a4522
Grant Wuerker <gwuerker@googlemail.com>
2018-11-03 15:29:08 -0500

    RecurringTransfersModule compiles

--- contracts/modules/RecurringTransfersModule.sol
+++ contracts/modules/RecurringTransfersModule.sol
@@ -13,8 +13,8 @@ contract RecurringTransfersModule is Module {
 
     DateTime dateTime;
 
-    // recurringTransfers maps a token and account address to a recurring transfer settings.
-    mapping ((address, address) => RecurringTransfer) public recurringTransfers;
+    // recurringTransfers maps the composite hash of a token and account address to a recurring transfer struct.
+    mapping (uint32 => RecurringTransfer) public recurringTransfers;
 
     struct RecurringTransfer {
         address fiat;
@@ -29,30 +29,43 @@ contract RecurringTransfersModule is Module {
         dateTime = new DateTime();
     }
 
+    function changeRecurringTransfer(address token, address receiver, address fiat, uint256 amount)
+        public
+    {
+        require(OwnerManager(manager).isOwner(msg.sender), "Method can only be called by an owner");
+        recurringTransfers[addressComposite(token, receiver)] = RecurringTransfer(fiat, amount, now);
+    }
+
     function executeRecurringTransfer(address token, address receiver)
         public
     {
         require(OwnerManager(manager).isOwner(msg.sender), "Method can only be called by an owner");
 
-        RecurringTransfer recurringTransfer = reccuringTransfers[(token, receiver)];
-        require(isNextMonth(recurringTransfer), "Transfer has already been executed this month")
+        RecurringTransfer memory recurringTransfer = recurringTransfers[addressComposite(token, receiver)];
+        require(isNextMonth(recurringTransfer.lastTransferTime), "Transfer has already been executed this month");
 
         if (token == 0) {
-            require(manager.execTransactionFromModule(to, amount, "", Enum.Operation.Call), "Could not execute ether transfer");
+            require(manager.execTransactionFromModule(receiver, recurringTransfer.amount, "", Enum.Operation.Call), "Could not execute ether transfer");
         } else {
-            bytes memory data = abi.encodeWithSignature("transfer(address,uint256)", to, amount);
+            bytes memory data = abi.encodeWithSignature("transfer(address,uint256)", receiver, recurringTransfer.amount);
             require(manager.execTransactionFromModule(token, 0, data, Enum.Operation.Call), "Could not execute token transfer");
         }
 
         recurringTransfer.lastTransferTime = now;
     }
 
-    function isNextMonth(RecurringTransfer recurringTransfer)
+    function addressComposite(address token, address receiver)
+        public pure returns (uint32)
+    {
+        return uint32(keccak256(token, receiver));
+    }
+
+    function isNextMonth(uint lastTransferTime)
         public view returns (bool)
     {
-        if (dateTime.getYear(now) > dateTime.getYear(reccuringTransfer.lastTransferTime) {
+        if (dateTime.getYear(now) > dateTime.getYear(lastTransferTime)) {
             return true;
-        } else if (dateTime.getMonth(now) > dateTime.getMonth(recurringTransfer.lastTransferTime)) {
+        } else if (dateTime.getMonth(now) > dateTime.getMonth(lastTransferTime)) {
             return true;
         }
 
