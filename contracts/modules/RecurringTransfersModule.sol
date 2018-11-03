a605a7b6f150d631af51fc73e07815803b0872cf
Grant Wuerker <gwuerker@googlemail.com>
2018-11-03 12:36:59 -0500

    RecurringTransferModule execute transaction filled out

--- contracts/modules/RecurringTransfersModule.sol
+++ contracts/modules/RecurringTransfersModule.sol
@@ -2,8 +2,8 @@ pragma solidity 0.4.24;
 import "../base/Module.sol";
 import "../base/ModuleManager.sol";
 import "../base/OwnerManager.sol";
-import "../common/Enum.sol";
 import "../common/DateTime.sol";
+import "../common/Enum.sol";
 
 /// @title Recurring Transfer Module - Allows an owner...
 /// @author Grant Wuerker - <gwuerker@gmail.com>
@@ -11,24 +11,48 @@ contract RecurringTransfersModule is Module {
     string public constant NAME = "Recurring Transfers Module";
     string public constant VERSION = "0.0.2";
 
-    address public token;
-    address public receiver;
-    uint256 public amount;
-
     DateTime dateTime;
-    uint private lastTransferTime;
+
+    // recurringTransfers maps a token and account address to a recurring transfer settings.
+    mapping ((address, address) => RecurringTransfer) public recurringTransfers;
+
+    struct RecurringTransfer {
+        address fiat;
+        uint256 amount;
+        uint lastTransferTime;
+    }
 
     function setup()
         public
     {
         setManager();
-        lastTransferTime = 0;
+        dateTime = new DateTime();
     }
 
-    function isNextMonth() private view returns (bool) {
-        if (dateTime.getYear(now) > dateTime.getYear(lastTransferTime)) {
+    function executeRecurringTransfer(address token, address receiver)
+        public
+    {
+        require(OwnerManager(manager).isOwner(msg.sender), "Method can only be called by an owner");
+
+        RecurringTransfer recurringTransfer = reccuringTransfers[(token, receiver)];
+        require(isNextMonth(recurringTransfer), "Transfer has already been executed this month")
+
+        if (token == 0) {
+            require(manager.execTransactionFromModule(to, amount, "", Enum.Operation.Call), "Could not execute ether transfer");
+        } else {
+            bytes memory data = abi.encodeWithSignature("transfer(address,uint256)", to, amount);
+            require(manager.execTransactionFromModule(token, 0, data, Enum.Operation.Call), "Could not execute token transfer");
+        }
+
+        recurringTransfer.lastTransferTime = now;
+    }
+
+    function isNextMonth(RecurringTransfer recurringTransfer)
+        public view returns (bool)
+    {
+        if (dateTime.getYear(now) > dateTime.getYear(reccuringTransfer.lastTransferTime) {
             return true;
-        } else if (dateTime.getMonth(now) > dateTime.getMonth(lastTransferTime)) {
+        } else if (dateTime.getMonth(now) > dateTime.getMonth(recurringTransfer.lastTransferTime)) {
             return true;
         }
 
